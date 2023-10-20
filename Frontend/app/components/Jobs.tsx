c12cb1c6951c49f3c27d84fc017181c32d3437a5
Amir kamal <133527168+0amirk@users.noreply.github.com>
2023-10-20 10:40:41 +0530

    Changed grid to flex

--- Frontend/app/components/Jobs.tsx
+++ Frontend/app/components/Jobs.tsx
@@ -76,7 +76,7 @@ const Jobs: React.FC<JobsProps> = ({ searchQuery }) => {
 
   return (
     <div className="container mx-auto px-3">
-      <div className="flex flex-col md:flex-row py-4 space-y-4 md:space-y-0 md:space-x-4">
+      <div className="grid grid-cols-1 md:grid-cols-4 py-4 space-y-4 md:space-y-0 md:space-x-4">
         {jobs.map(job => (
           <div key={job['_id']} className="bg-[#121212] w-full md:w-1/4 rounded-lg p-4 text-white">
             <div>
